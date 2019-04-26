4979ee5fba9a36a0ad8c475f8821259d160d8eb1
marcin <coreboot@tlen.pl>
2019-04-26 22:12:30 +0200

    takie tam

--- app/src/main/java/com/example/test_shd/MainActivity.kt
+++ app/src/main/java/com/example/test_shd/MainActivity.kt
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+var btn_state : Boolean = false
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener{ button.text = getString(R.string.btn_clicked) }
+        button.setOnClickListener {
+            when (btn_state) {
+                false -> button.text = getString(R.string.btn_clicked)
+
+                true -> button.text = getString(R.string.btn_unclicked)
+
+            }
+            btn_state = !btn_state
+        }
     }
 }
